a4c1a3188b9063344c855925c832abbd287d5401
kive7791 <kvel1097@gmail.edu>
2024-10-26 23:18:16 -0600

    main menu changed

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,11 +1,22 @@
 extends CanvasLayer
 
+class_name ui 
+signal game_start
+var game_level = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var end = $EndScreen
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
 	pass
+	
+
+func update_level(level: int):
+	game_level = level
+	
+
+func on_game_over():
+	end.visible = true
+	$EndScreen/Score/LevelReahced.text = '%d' % game_level
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
